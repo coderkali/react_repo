b7ddf4f657be757a242ed16104ee828f57a9aa46
Kpadhi <Evoke@0921013053>
2020-11-17 21:47:47 +0530

    first react app

---
+++ firstapp/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+
+
+ReactDOM.render(
+   <React.Fragment> 
+       <h1>Hello World!</h1>
+       <h2>Hello World!</h2>
+    </React.Fragment>,
+
+    //  <div>
+    // <h1>Hello World!</h1>
+    // <h2>Hello World!</h2>
+    // </div>,
+
+    // [
+    // <h1>Hello World!</h1>,
+    // <h2>Hello World!</h2>
+    // ],
+    document.getElementById('root'));
+
+
+// var h1 = document.createElement('h1');
+// h1.innerHTML ='Hello World!';
+// document.getElementById('root').appendChild(h1);
