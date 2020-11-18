a9179c68a4e80cfb8f1758225f489f5333ac0e26
Kpadhi <coderkali@gmail.com>
2020-11-18 22:09:28 +0530

    more react feature added

--- firstapp/src/index.js
+++ firstapp/src/index.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import './index.css'
 
+
+const msg = "This Is React Js First App";
+const fname ="Kali";
+const lname ="Prasad";
+
+const currentDate = new Date().toLocaleDateString();
+const currentTime = new Date().toLocaleTimeString();
+
+const img1 = "https://picsum.photos/200/300"
+const img2 = "https://picsum.photos/200/300"
+const img3 = "https://picsum.photos/200/300"
+
+const links = "https://google.com"
 
 
 ReactDOM.render(
    <React.Fragment> 
-       <h1>Hello World!</h1>
-       <h2>Hello World!</h2>
+       <h1 className="heading">My Name is {fname} {lname}</h1>
+       <h1>My Name is {fname+ " "+lname}</h1>
+       <h1>My Name is {` ${fname} ${lname}`}</h1>
+       <h1>Current Date is  {currentDate}</h1>
+       <h1>Current Time is  {currentTime}</h1>
+       <h1 contentEditable="true">Hello World!</h1>
+       
+       <div className="img_div">
+         <img src={img3} alt="just img"/>
+         <img src={img1} alt="just img"/>
+         <img src={img2} alt="just img"/>
+         <a href ={links} target="_target">
+            <img src={img3} alt="just img"/>
+         </a>
+       </div>
+       
+       
+       <h2>{msg}</h2>
+       <p>2+ 3 is {2+3}</p>
+       <p>My Lucky Number is {Math.random()}</p>
     </React.Fragment>,
 
     //  <div>
