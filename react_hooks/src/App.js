1947fa2d5aca6607ec8bbd47ace383bcd035c42f
Kpadhi <coderkali@gmail.com>
2020-11-28 10:19:09 +0530

    React hooks code added

--- react_hooks/src/App.js
+++ react_hooks/src/App.js
@@ -1,10 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+
+
+
+
+const App = () => {
+
+   let newTime = new Date().toLocaleTimeString();
+
+   const [ctime,setCtime] = useState(newTime);
+
+   const [currentTime,setCurrentTime] = useState(newTime);
+
+   const [count, setCount] = useState(0);
+
+   const IncNum = () => {
+      newTime = new Date().toLocaleTimeString();
+      setCtime(newTime);
+      setCount(count +1);
+   };
+
+   const updateTime = () => {
+    let time = new Date().toLocaleTimeString();
+    setCurrentTime(time);
+   }
+
+   setInterval(updateTime, 1000);
+
+
   return (
-    
+    <>
+      <h1> Current Time :: {currentTime}</h1>
+      
+      <h1> {ctime} </h1>
+      <h1> {count} </h1>
+      <button onClick={IncNum}>Click Me</button>
+    </>
   );
-}
+};
 
 export default App;
